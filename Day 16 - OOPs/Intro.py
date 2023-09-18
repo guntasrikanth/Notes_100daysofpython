ec1e3bb17952e4d62bc19564cf41930f0a30d33c
guntasrikanth <guntasrikanth@gmail.com>
2023-09-18 18:16:33 +0530

    Update Intro.py

--- Day 16 - OOPs/Intro.py
+++ Day 16 - OOPs/Intro.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-jimmy = Turtle()
+''''jimmy = Turtle()
 jimmy.shape("turtle")
 jimmy.color("red")
 for steps in range(100):
@@ -9,7 +10,11 @@ for steps in range(100):
         jimmy.forward(steps)
         jimmy.right(30)
 
-
-
 my_screen = Screen()
-my_screen.exitonclick()
+my_screen.exitonclick() '''
+
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'              
+print(table)
