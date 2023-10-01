659726e386e126c7c13167160ad8063ffa2aa053
guntasrikanth <guntasrikanth@gmail.com>
2023-10-01 22:08:27 +0530

    day 23 added

--- Day 23 - files/main.py
+++ Day 23 - files/main.py
@@ -10,18 +10,26 @@
 #     print(contents)
 
 
-#opening file in write mode
+# opening file in write mode
 # with open('my_file.txt', mode='w') as file:
 #     file.write('\nThis is Srikanth.')
 
 
-#opening file in append mode
+# opening file in append mode
 # with open('my_file.txt', mode='a') as file:
 #     file.write('\nThis is Srikanth.')
 
 
-#opening file in write mode, creates new file if provided file doesn't  exists
-with open('my_new_file.txt', mode='w') as file:
-    file.write('\nThis is Srikanth.')
+# opening file in write mode, creates new file if provided file doesn't exists
+# with open('my_new_file.txt', mode='w') as file:
+#     file.write('\nThis is Srikanth.')
 
+# opening file in absolute path
+# with open("/Users/sgunta/Desktop/my_new_file.txt", mode='a') as file:
+#     file.write('\nI live in Hyderabad.')
+#     #print(file.read())
 
+# opening file in relative path
+with open("../../../../Desktop/my_new_file.txt", mode='r') as file:
+    # file.write('\nI live in Hyderabad.')
+    print(file.read())
